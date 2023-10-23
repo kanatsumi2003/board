ff92a18252b60458f61edb1925df7cd460c3f1b8
trungtin1811 <se.nttin1811@gmail.com>
2023-10-23 10:37:33 +0700

    polling get locker info

--- locker-app/src/pages/maintaining/index.tsx
+++ locker-app/src/pages/maintaining/index.tsx
@@ -1,6 +1,24 @@
 import LockerMaintaining from "@/assets/locker_maintaining.png";
-
+import {
+  LOCKER_INFO_POLLING_INTERVAL,
+  PATH,
+  PAYMENT_POLLING_INTERVAL,
+} from "@/constants/common";
+import { useLockerInfoQuery } from "@/services/boardService";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function MaintainPage() {
+  const navigate = useNavigate();
+  const { data, isSuccess, isFetching } = useLockerInfoQuery(undefined, {
+    pollingInterval: LOCKER_INFO_POLLING_INTERVAL,
+  });
+
+  useEffect(() => {
+    if (isSuccess && !isFetching && data) {
+      navigate(PATH.HOME);
+    }
+  }, [isSuccess, isFetching]);
+
   return (
     <div className="p-10 h-full flex flex-col gap-12 justify-center items-center text-center">
       <img
@@ -8,7 +26,7 @@ function MaintainPage() {
         alt=""
         className="object-scale-down h-[400px]"
       />
-      <div className="text-2xl font-bold">
+      <div className="font-bold">
         Locker hiện đang tạm ngưng để bảo trì <br /> Bạn vui lòng thử lại sau.
       </div>
     </div>
--- locker-app/src/pages/offline/index.tsx
+++ locker-app/src/pages/offline/index.tsx
@@ -4,7 +4,7 @@ function MaintainPage() {
   return (
     <div className="p-10 h-full flex flex-col gap-12 justify-center items-center text-center">
       <img src={Offline} alt="" className="object-scale-down h-[500px]" />
-      <div className="text-2xl font-bold">
+      <div className="font-bold">
         Locker hiện đang đang không có kết nối mạng <br /> Bạn vui lòng thử lại
         sau.
       </div>
