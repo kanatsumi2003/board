11275877b7dd44cd9b4fd84ccaf9d7732be39b33
leson131002 <leson131002@gmail.com>
2023-10-31 23:35:41 +0700

    Fix create super user

--- script-config.bat
+++ script-config.bat
@@ -11,6 +11,6 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@gmail.com', 'Aqswde123@')" | python manage.py shell
+echo from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@gmail.com', 'Aqswde123@') | python manage.py shell
 
 python  -d manage.py runserver 0.0.0.0:8000
