7efea5460ffe98859effd5334c029314723b05c9
Tin Nguyen <se.nttin1811@gmail.com>
2023-12-07 22:02:58 +0700

    fix: ui

--- locker-app/src/components/receive/ReceiveOrderDetail.tsx
+++ locker-app/src/components/receive/ReceiveOrderDetail.tsx
@@ -4,6 +4,7 @@ import { IDetailItem, ORDER_TYPE } from "@/interfaces/order";
 import { useCheckOutOrderMutation } from "@/services/orderService";
 import { AppState } from "@/stores";
 import { formatCurrency, formatDate } from "@/utils/formatter";
+import { renderOrderTypeText } from "@/utils/orderTypeRender";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Title from "../Title";
@@ -25,7 +26,6 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
     useCheckOutOrderMutation();
 
   const modal = useModal();
-
   if (!order) {
     return <></>;
   }
@@ -54,15 +54,19 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
   return (
     <>
       <Title subtitle="Nhận hàng">Xác nhận thông tin đơn hàng</Title>
-      <div className="mt-52 flex w-full items-center flex-col h-full justify-between px-12">
+      <div className="mt-44 flex w-full items-center flex-col h-full justify-between px-12">
         <div className="w-full">
           <div className="text-center">
             <div className="font-light">Số tiền cần thanh toán</div>
             <div className="text-7xl font-bold mt-4">
-              {formatCurrency(order.totalPrice ?? 0)}
+              {formatCurrency(
+                order.totalPrice - order.reservationFee > 0
+                  ? order.totalPrice - order.reservationFee
+                  : 0
+              )}
             </div>
           </div>
-          <div className="flex flex-col gap-12 w-full justify-center mt-12">
+          <div className="flex flex-col gap-8 w-full justify-center mt-8">
             <Card className="grid grid-cols-2 gap-y-3 gap-x-1 basis-3/5 justify-center">
               <div className="font-semibold col-span-2 mb-4 text-4xl">
                 Thông tin đơn hàng:
@@ -72,7 +76,9 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
               <div>Mã PIN đơn hàng:</div>
               <div className="font-bold text-end">{order.pinCode}</div>
               <div>Loại dịch vụ:</div>
-              <div className="font-bold text-end">{order.type}</div>
+              <div className="font-bold text-end">
+                {renderOrderTypeText(order.type)}
+              </div>
               <div>SĐT người gửi:</div>
               <div className="font-bold text-end">
                 {order.sender.phoneNumber}
@@ -87,7 +93,7 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
               )}
               {order.createdAt && (
                 <>
-                  <div>Thời gian:</div>
+                  <div>Thời gian tạo đơn hàng:</div>
                   <div className="font-bold text-end">
                     {formatDate(order.createdAt)}
                   </div>
@@ -110,7 +116,7 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
                   </div> */}
                   <div>Trả trước:</div>
                   <div className="font-bold text-end">
-                    - {formatCurrency(order.reservationFee ?? 0)}
+                    {formatCurrency(order.reservationFee ?? 0)}
                   </div>
                   <div>Phụ thu:</div>
                   <div className="font-bold text-end">
@@ -123,6 +129,16 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
                       ""
                     )}
                   </div>
+                  {order.reservationFee > order.totalPrice && (
+                    <>
+                      <div>Hoàn trả:</div>
+                      <div className="font-bold text-end">
+                        {formatCurrency(
+                          order.reservationFee - order.totalPrice ?? 0
+                        )}
+                      </div>
+                    </>
+                  )}
                 </>
               )}
               {order.type === ORDER_TYPE.STORAGE && (
@@ -130,10 +146,26 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
                   <div className="font-semibold col-span-2 my-4 text-4xl">
                     Chi tiết hóa đơn:
                   </div>
+                  <>
+                    <div>Tổng tiền:</div>
+                    <div className="font-bold text-end">
+                      {formatCurrency(order.totalPrice ?? 0)}
+                    </div>
+                  </>{" "}
                   <div>Trả trước:</div>
                   <div className="font-bold text-end">
-                    - {formatCurrency(order.reservationFee ?? 0)}
+                    {formatCurrency(order.reservationFee ?? 0)}
                   </div>
+                  {order.reservationFee > order.totalPrice && (
+                    <>
+                      <div>Hoàn trả:</div>
+                      <div className="font-bold text-end">
+                        {formatCurrency(
+                          order.reservationFee - order.totalPrice ?? 0
+                        )}
+                      </div>
+                    </>
+                  )}
                 </>
               )}
             </Card>
@@ -144,7 +176,7 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
                     {`Chi tiết đơn hàng (${order.details.length} dịch vụ):`}
                   </div>
                   <Draggable>
-                    <div className="flex flex-col overflow-y-scroll h-[280px] gap-2 w-full">
+                    <div className="flex flex-col overflow-y-scroll h-[240px] gap-2 w-full">
                       {order.details.map((detail: IDetailItem) => (
                         <div
                           className={`flex p-2 bg-gray-100 rounded-lg w-full items-center md:h-52 gap-8`}
@@ -208,18 +240,18 @@ function ReceiveOrderDetail({ onNext, onPrev }: Props) {
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <div>
+          {/* <div>
             <Asterisk /> Số tiền trên chỉ bao gồm phí dịch vụ (chưa bao gồm số
             tiền trả trước{" "}
             <TextBold>
               {formatCurrency(orderSettings?.reservationFee ?? 0)}
             </TextBold>
             ).
-          </div>
+          </div> */}
           <div>
             <Asterisk /> Sau khi ấn xác nhận, hệ thống sẽ tự động kiểm tra và{" "}
             <TextBold>tự động thanh toán</TextBold>, đồng thời cũng hoàn trả lại{" "}
-            <TextBold>tiền trả trước</TextBold> vào số dư ví của bạn
+            <TextBold>tiền trả trước</TextBold> vào số dư ví của bạn.
           </div>
           <Button
             type="primary"
